5a3da103a9100c703a13325b5c7ef392ecd331a6
olliehub <you@example.com>
2022-12-09 14:32:39 -0800

    sizing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,12 @@
  *
  */
 int main(void)
-
 {
 	int a;
 	long int b;
 	long long int c;
 	char d;
 	float f;
-	
 	printf("Size of a char: %lu byte(s)\n", 
 			(unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", 
@@ -25,5 +23,4 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", 
 			(unsigned long)sizeof(f));
 	return (0);
-
 }
