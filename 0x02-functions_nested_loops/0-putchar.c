9b1fadcf314327bcf353ab7fa50b53bd483b375d
olliehub <you@example.com>
2022-12-16 00:01:11 -0800

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,13 @@ int main(void)
 {
         char string[8] = "_putchar";
 	int n = 0;
-
 	while (n < 8)
 	{
-                _putchar(string[n]);
-               ++n;
-        }
 
-      _putchar(10);
-      return (0);
+		_putchar(string[n]);
+		++n;
+	}
 
+	_putchar(10);
+	return (0);
 }
