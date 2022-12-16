2fa661f23060573d3625fac2f5a8911f2cc7d133
olliehub <you@example.com>
2022-12-15 23:17:47 -0800

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * main - main function
-  *
-   * Return: End program
-    */
+* main - main function
+*
+* Return: End program
+*/
 int main(void)
 {
         char string[8] = "_putchar";
 	        int n = 0;
 
-		        while (n < 8)
-				        {
-					                _putchar(string[n]);
-							                ++n;
-									        }
+	while (n < 8)
+	{
+                _putchar(string[n]);
+               ++n;
+        }
 
-			        _putchar(10);
-				        return (0);
+      _putchar(10);
+      return (0);
 
-}                       ))])}>)""]})""
+}
