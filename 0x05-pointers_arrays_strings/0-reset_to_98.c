3ad43f233d6979a7941941c7ccc8eab5c865fd73
olliehub <you@example.com>
2022-12-20 15:49:13 -0800

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,11 +4,14 @@
   * reset_to_98 - function
   *
   * Return: Always 0
-  *
   */
-void reset_to_98(int *n)
+void reset_to_98(int *n);
 {
-	*n = 98
+	*n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n)
+	printf("%d\n", n);
+	return(0);
 
 }
 
