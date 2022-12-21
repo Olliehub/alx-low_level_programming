26161b8149152b51fef03cf0beea3151ee99f199
olliehub <you@example.com>
2022-12-20 16:03:55 -0800

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+
+void reset_to_98(int *n);
 /**
   * reset_to_98 - function
   *
+  *@n: pointer of a variable
+  *
   * Return: Always 0
   */
-void reset_to_98{int *n};
+int main(void);
 {
-	*n = 402;
+	*n = (402);
 	printf("%d\n", n);
-	reset_to_98(&n)
+	reset_to_98(&n);
 	printf("%d\n", n);
 	return(0);
 
