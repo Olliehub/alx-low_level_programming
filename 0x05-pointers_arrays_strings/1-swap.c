563fb2f8d395b5e6dc9d852293c20be4bb4a3bbc
olliehub <you@example.com>
2022-12-21 00:36:30 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * swap_int - function
+  *
+  * @a: pointer of integer
+  * @b: pointer of integer
+  */
+void swap_int(int *a, int *b);
+{
+	int *a = 0;
+	(*a) = (*b);
+	(*b) = (*a);
+
+}
