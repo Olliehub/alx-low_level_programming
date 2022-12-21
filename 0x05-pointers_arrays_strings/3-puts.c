a38629a9db216ca2ec0829d9317f9cdcbf9ebb99
olliehub <you@example.com>
2022-12-21 06:52:04 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _puts - prints a string followed by a new line
+  *
+  * @str: pointerof a string
+  *
+  * Return: nothing
+  */
+void _puts(char *str)
+{
+	int n = 0;
+
+	while (*(str + n) != '\0')
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
