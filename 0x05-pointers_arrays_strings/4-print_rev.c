2ffab86c440c117f51abd60eaaa8b49229aff770
olliehub <you@example.com>
2022-12-21 21:13:04 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string in reverse
+  *
+  * @s: pointer of a parameter
+  */
+void print_rev(char *s);
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+	while (length > 0)
+		_putchar(s[--length]);
+	_putchar(10);
+}
