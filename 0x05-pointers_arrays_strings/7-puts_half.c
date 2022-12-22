8f7c42cd6724313899464be61673bd771209a4c9
olliehub <you@example.com>
2022-12-22 11:37:40 -0800

    7puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *
+  * puts_half - prints half of a string
+  *
+  * @str: pointer of a parameter
+  */
+void puts_half(char *str)
+{
+	int length = 0;
+	int sublength;
+
+	while (str[length] != '\0')
+		length++;
+	sublength = (length % 2 == 0) ? length / 2 : (length + 1) / 2;
+	while (str[sublength] != '\0')
+		_putchar(str[sublength++]);
+	_putchar(10);
+}
