45acf61bf50aa6b5f19f6ed832c2af0336a435e2
olliehub <you@example.com>
2023-01-04 06:34:48 -0800

    pointers and functions

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,9 +5,9 @@
   *
   * @n: buffer size
   * @s: bytes of the memory area pointed
-  * @b: thr constant byte
+  * @b: the constant byte
   *
-  * Return: pointer to memory area s
+  * Return: pointer to memory area s.
   */
 char *_memset(char *s, char b, unsigned int n)
 {
