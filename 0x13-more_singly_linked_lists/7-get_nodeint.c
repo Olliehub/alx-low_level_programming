df224c0a33163d8bc6b53f921be0aec992fcd244
olliehub <charlibossubom@gmail.com>
2023-05-01 06:18:52 -0700

    function that returns all the data (n)

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,12 +10,17 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *new = head;
 	unsigned int i;
 
-	for (i = 0, i < index && head != NULL; i++;)
+	if (head == NULL)
+		return (NULL);
+	for (i = 0, i < index && new; i++;)
 	{
-		head = head->next;
+		if (new->next == NULL)
+			return (NULL);
+		new = new->next;
 	}
 
-	return (head);
+	return (new);
 }
